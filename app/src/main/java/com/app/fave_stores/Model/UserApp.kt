97163571c63bf4f041e413d4c6ca131714a14888
Laura-Oliveira <laura.regina.cp@gmail.com>
2023-11-08 22:17:27 -0300

    [Architecture] [Login] Definition of the MVC Architecture | Implementation depedencies on gradle files | Starting implemention login task

---
+++ app/src/main/java/com/app/fave_stores/Model/UserApp.kt
@@ -0,0 +1,31 @@
+package com.app.fave_stores.Model
+
+public class UserApp
+{
+    private lateinit var idUser: String
+    private lateinit var username: String
+    private lateinit var email: String
+    private lateinit var phone: String
+    private lateinit var password: String
+
+    fun getId(): String
+    { return idUser }
+
+    fun setId(idUser: String) { this.idUser = idUser }
+
+    fun getUserName(): String { return username }
+
+    fun setUserName(name: String) { this.username = username }
+
+    fun getEmail(): String { return email }
+
+    fun setEmail(email: String) { this.email = email }
+
+    fun getPhone(): String { return phone }
+
+    fun setPhone(phone: String) { this.phone = phone }
+
+    fun getPassword(): String { return password }
+
+    fun setPassword(password: String) { this.password = password }
+}
---
+++ app/src/main/java/com/app/fave_stores/View/LoginActivity.kt
@@ -0,0 +1,86 @@
+
+package com.app.fave_stores.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.fave_stores.R
+import android.content.Intent
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity()
+{
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var txt_email: String
+    private lateinit var txt_password:String
+
+    private lateinit var emailEditText:EditText
+    private lateinit var passwordEditText:EditText
+    private lateinit var btnLogin: Button
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        FirebaseApp.initializeApp(this)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        emailEditText = findViewById(R.id.edit_text_login_email)
+        passwordEditText = findViewById(R.id.edit_text_login_password)
+        btnLogin = findViewById(R.id.btn_login)
+
+        btnLogin.setOnClickListener {
+            txt_email = emailEditText.text.toString()
+            txt_password = passwordEditText.text.toString()
+
+            if ((txt_email.isNotEmpty() || txt_email.isNotBlank()) && (txt_password.isNotEmpty() || txt_password.isNotBlank()))
+            {
+                loginUser(txt_email, txt_password)
+            }
+            else
+            { Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show() }
+        }
+    }
+
+
+/* Authentication with users registered directed on the app */
+
+    private fun loginUser(email: String, password: String)
+    {
+        if (email.isNotEmpty() && password.isNotEmpty())
+        {
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                if (it.isSuccessful)
+                {
+                    val intent = Intent(this, RegisterUserActivity::class.java)
+                    Toast.makeText(this, "Login Sucsessful!", Toast.LENGTH_SHORT).show()
+                    startActivity(intent)
+                    finish()
+                }
+                else
+                {
+                    Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    Log.d("Error Login", it.exception.toString())
+                }
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "Empty Fields Are not Allowed!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun openRegisterUserPage(view:View?)
+    {
+        val activity = Intent(this, RegisterUserActivity::class.java)
+        startActivity(activity)
+    }
+
+}
